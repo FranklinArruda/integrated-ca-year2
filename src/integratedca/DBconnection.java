252333ca7d42acc9f98a28d298ef0cfa2e638a2a
unknown <franklin.arrudaa@gmail.com>
2022-12-14 01:55:57 +0000

    No progress
    Changing all over...

--- src/integratedca/DBconnection.java
+++ src/integratedca/DBconnection.java
@@ -1,6 +1,7 @@
 package integratedca;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -8,152 +9,98 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-/**
- *
- * @author Student
- */
 public class DBconnection {
+    Connection conn=null;
 
-    // Admin User Pre-registered Credentials as per Assignment Requirmeents...
     static final String DB_NAME = "integratedca";
     static final String DATABASE_URL = "jdbc:mysql://localhost/" + DB_NAME;
-    static final String USERNAME = "CCT";
-    static final String PASSWORD = "Dublin";
+    static final String USERNAME = "root";
+    static final String PASSWORD = "root";
     static final String driver = "com.mysql.cj.jdbc.Driver";
     static final String TB_NAME = "user";
 
-  // Default constructor 
-  public DBconnection() {
+    public DBconnection() {
 
         try {
-          Class.forName(driver).newInstance();
-          Connection conn = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
-          Statement stmt = conn.createStatement();
-          stmt.execute("USE " + DB_NAME + ";");
+            Class.forName(driver).newInstance();
+            Connection conn = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+            Statement stmt = conn.createStatement();
+            stmt.execute("USE " + DB_NAME + ";");
         } catch (Exception e) {
-          System.out.println("h.");
-          e.printStackTrace();
+            System.out.println("Failed to connect");
+            e.printStackTrace();
         }
-      };
+    }
 
-  /**
-   * @Boolean method to load the DRIVER to the database 
-   * @return true if connection is successful
-   * @return false if connection fails
-   */
-  static public boolean DBsetUp() {
-        System.out.println("DBsetUp");
-        //try-catch for driver instance
-        try {
-          Class.forName(driver).newInstance();
-        } catch (Exception e) {
-          System.out.println("Connection Failed. Please check your Driver method.");
-          e.printStackTrace();
+     
+    
+    
+    // !!! insert where name=name AND pass=pass
+    public boolean readUser(String email_, String pass) throws SQLException {
+            
+       Statement stmt = conn.createStatement();
+ 
+        String select= "SELECT * FROM user WHERE email="+email_+", password;"+pass;
+        String SQL="SELECT * FROM user WHERE email= " + email_ + "password= " + pass + ";";
+        System.out.print(select);
+        ResultSet rs = stmt.executeQuery(select);
+        
+        Boolean found = false;
+        
+        while (rs.next()){
+            found = true;
+            String email = rs.getString(1);
+            String name = rs.getString(2);
+            String surname = rs.getString(4);
+            String city = rs.getString(5);
+            String country = rs.getString(6);
+            System.out.println(email + "\t" + "\t" + name);
         }
+        return found;
+    }
 
-        // try-catch for connection                   
-        try {
-          Connection conn = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
-          Statement stmt = conn.createStatement();
-          // INSERTING data into database
-          stmt.execute("USE " + DB_NAME + ";");
-          stmt.executeUpdate(
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // registering new User
+    public String registerUser(String name, String pass) throws SQLException, IOException {
 
-            // FIND A EASIER WAY OF INSERT DATA USING JAVA
-            String.format("INSERT INTO " + TB_NAME + "(user_name,user_passWord)"
-              //FIX THIS LINE...
-              +
-              "VALUES (\"%s\",\"%s\") ;"
-              // using the 'User' arguments to get the variable attribute
-              //in the class 'Users' and set values to be called in the main...
-            ));
-
-          //DISPLAYING ALL DATA CREATED FOR TESTING PORPUSES...
-          ResultSet results = stmt.executeQuery("SELECT * FROM " + DB_NAME + "." + TB_NAME + ";");
-
-          while (results.next())
-            System.out.println("Username: " + results.getString(1) + ", ID " + " " + results.getString(2) + ", Pass " + " " + results.getString(3));
-          conn.close();
-          // System.out.println(rs);
-
-          return true;
-        } catch (SQLException e) {
-          System.out.println("Something Went wrong in Insert method. please check the following ERROR!!!");
-          e.printStackTrace(); // Will show the exact line in which the method raised the exception
-        }
-        return false;
-      }
-
-  /**
-   * @return false if not authentication user is correct
-   * @return true if user athentication is successful
-   */
-  public boolean Login() {
-
-        BufferedReader myKeyboard = new BufferedReader(new InputStreamReader(System.in));
-        String userName = "", password = "";
-        do {
-          try {
-            System.out.print(" Enter user name => ");
-            userName = myKeyboard.readLine();
-
-            System.out.print(" Enter password => ");
-            password = myKeyboard.readLine();
-
-          } catch (Exception e) {
-            System.out.println("PUT SOMETHING USEFUL HERE..."); // DONT FORGET
-          }
-
-          if (USERNAME.equals(userName) && PASSWORD.equals(password)) {
-            System.out.println(" User successfully logged-in.. ");
-            if (true) {
-              System.out.println("Say now. WHO IS THE BOSS.. HAHA");
-              /*
-                  do stuff do tother stuff
-                  call the methods and things*/
-            }
-
-          } else {
-            System.out.println("Invalid userName of password");
-            System.out.println("Please try again.");
-            System.out.println("If not user yet, sigh-up...");
-            DBconnection Register = new DBconnection();
-            Register.Register();
-          }
-        } while (!USERNAME.equals(userName) && (!PASSWORD.equals(password)));
-        //}
-
-        return false;
-  }
-
-  public String Register() {
-        User Register = new User("");
         BufferedReader kb = new BufferedReader(new InputStreamReader(System.in));
-        String userFName = "", userLName = "", userName = "", password = "";
+        String firstName = "", lastName = "", password = "";
 
-        try {
-          System.out.print("Enter your First Name: ");
-          userFName = kb.readLine();
-          Register.setUserFirstName(userFName);
+        System.out.print("Enter your First Name: ");
+        firstName = kb.readLine();
 
-          // Setting Last Name
-          System.out.print("Enter your Last Name: ");
-          userLName = kb.readLine();
-          Register.setUserFirstName(userLName);
+        // Setting Last Name
+        System.out.print("Enter your Last Name: ");
+        lastName = kb.readLine();
 
-          // Setting Username
-          System.out.print("Enter your Username: ");
-          userName = kb.readLine();
-          Register.setUserFirstName(userName);
+        // Setting password
+        System.out.print("Enter your password: ");
+        password = kb.readLine();
 
-          // Setting password
-          System.out.print("Enter your password: ");
-          password = kb.readLine();
-          Register.setUserFirstName(password);
-        } catch (Exception e) {
-          System.out.println("something went wrong...");
-
-        }
+        // !!! write code here to check if the user already exists meaning there is no other user with firstName, LastName equal.
+        // if user doesn't exist insert the record on the database.s
+        Statement stmt = conn.createStatement();
+        stmt.executeUpdate(
+                // FIND A EASIER WAY OF INSERT DATA USING JAVA
+                String.format("INSERT INTO user (user_name, user_passWord)"
+                        //FIX THIS LINE...
+                        + "VALUES (\"%s\",\"%s\") ;"
+                // using the 'User' arguments to get the variable attribute
+                //in the class 'Users' and set values to be called in the main...
+                ));
         return null;
+
     }
 }
--- src/integratedca/User.java
+++ src/integratedca/User.java
@@ -1,52 +1,41 @@
 package integratedca;
+
+import static integratedca.IntegratedCA.db;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.SQLException;
+
 public class User {
+    private String email; // attributes
+    private String name;
+    private String surname;
 
-  private String firstName; // attributes
-  private String lastName;
-  private String password;
-  private int id;
+    public User(String email, String name, String surname) {
+        this.email = email; 
+        this.name = name; 
+        this.surname = surname;
+    }
+    
+   
+    public static boolean login() throws SQLException {
 
-  public User(String userRecord) { // Default constructor 
+        BufferedReader myKeyboard = new BufferedReader(new InputStreamReader(System.in));
+        String email = "", password = "";
 
-    this.firstName = userRecord; // change the fields user table removing (user_name => name) and so on. 
-    this.lastName = userRecord;
-    this.password = userRecord;
+        try {
+            System.out.print("Enter email: ");
+            email = myKeyboard.readLine();
 
-    //...
-    //  this.showMenu()// create menu user that shows the options as per requirements in the Project
-    // each menu option, creates a method (update name); replace new userName; updatae user record for userName; return true =updated;
-    //STEP 4:         
+            System.out.print("Enter password: ");
+            password = myKeyboard.readLine();
 
-  };
-
-  //STEP 5:
-  //PUBLIC SHOW MENU
-  public String getUserFirstName() {
-    return firstName;
-  }
-
-  public void setUserFirstName(String userFName) {
-    this.firstName = userFName;
-  }
-
-  public String getUserLastName() {
-    return lastName;
-  }
-
-  public void setUserLastName(String userLName) {
-    this.lastName = userLName;
-  }
-
-  public String getPassWord() {
-    return password;
-  }
-
-  public void setPassWord(String pass) {
-    this.password = pass;
-  }
-
-  public int getId() { // only get id as it is auto increment
-    return id;
-  }
-
+        } catch (Exception e) {
+            System.out.println("PUT SOMETHING USEFUL HERE..."); // DONT FORGET
+                 }
+        
+        db.readUser(email, password);
+        
+        return true;  
+    } 
 }
+ 
